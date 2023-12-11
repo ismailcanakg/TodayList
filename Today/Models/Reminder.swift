645fd8ccb935bce2f48fa440ed377705881229fd
İsmail Can Akgün <ismailcanakg1@gmail.com>
2023-12-11 19:05:02 +0300

    TodayList'e dair her şey açıklamalı şekilde

---
+++ Today/Models/Reminder.swift
@@ -0,0 +1,15 @@
+//
+//  Reminer.swift
+//  Today
+//
+//  Created by İsmail Can Akgün on 11.12.2023.
+//
+
+import Foundation
+
+struct Reminer {
+    var title: String
+    var dueDate: Date
+    var notes: String? = nil
+    var isComplate: Bool = false
+}
