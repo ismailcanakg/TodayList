123fbcbde2b6017819f728a124afbdcf997630b1
İsmail Can Akgün <ismailcanakg1@gmail.com>
2023-12-12 17:05:05 +0300

    ir hatırlatıcı görünümü oluşturduk.

--- Today/DetailViewController/ReminderViewController+Row.swift
+++ Today/DetailViewController/ReminderViewController+Row.swift
@@ -5,4 +5,40 @@
 //  Created by İsmail Can Akgün on 12.12.2023.
 //
 
-import Foundation
+import UIKit
+
+extension ReminderViewController {
+    // Ayrıntı görünümünde dört ayrı hatırlatma detayı göstereceksiniz: başlık, tarih, saat ve notlar.
+    //Daha sonra ayrıntı listesindeki öğeleri temsil etmek için Row örneklerini kullanacaksınız. UIKit listelerine veri ve stil sağlayan farklı veri kaynakları, öğelerin Hashable'a uygun olmasını gerektirir. Fark edilebilir veri kaynağı, anlık görüntüler arasında hangi öğelerin değiştiğini belirlemek için karma değerlerini kullanır.
+    enum Row: Hashable {
+        case date
+        case notes
+        case time
+        case title
+        
+        // Her durum için uygun bir SF Sembolü adı döndüren imageName adlı hesaplanmış bir özellik ekleyin.
+        var imageName: String? {
+            switch self {
+            case .date: return "calender.circle"
+            case .notes: return "square.and.pencil"
+            case .time: return "clock"
+            default: return nil
+            }
+        }
+        // Önceki bölümde, bir koleksiyon görünümünü liste olarak yapılandırmak için bir yapılandırma nesnesi kullandınız. Benzer şekilde, SF Sembolü görüntülerinin stilini yapılandırmak için bir sembol yapılandırma nesnesi kullanabilirsiniz.
+        // UIImage.Symbol'ınızda, sembol resminize uygulamak için metin ağırlığı, yazı tipi, nokta boyutu ve ölçek dahil olmak üzere stil girişlerini ekleyebilirsiniz. Sistem, görüntünün hangi varyantının kullanılacağını ve görüntünün nasıl biçimleneceğini belirlemek için bu ayrıntıları kullanır.
+        var image: UIImage? {
+            guard let imageName = imageName else { return nil }
+            let configuration = UIImage.SymbolConfiguration(textStyle: .headline)
+            return UIImage(systemName: imageName, withConfiguration: configuration)
+        }
+        
+        // Her hatırlatıcının başlığını vurgulamak için başlık yazı tipini kullanacaksınız.
+        var textStyle: UIFont.TextStyle {
+            switch self {
+            case .title: return .headline
+            default: return .subheadline
+            }
+        }
+    }
+}
--- Today/Models/Date+Today.swift
+++ Today/Models/Date+Today.swift
@@ -12,70 +12,55 @@ import Foundation
 extension Date {
     // dayAndTimeText adında hesaplanmış bir dize özelliği oluşturun.
     var dayAndTimeText: String {
-        /*
-         Olumsuz
+        /* NOT
          Siz dayAndTimeText için bir dize değeri döndürene kadar derleyici bir hata üretir.
-
          Swift Date değeri hem tarihi hem de saati içerir.
-         
          Saatin dize temsilini oluşturmak için formatted(date:time:) yöntemini çağırın ve sonucu timeText adlı bir sabite atayın.
+         Sistem, varsayılan bir stil kullanarak geçerli yerel ayar için tarih ve saatin dize gösterimini biçimlendirir. Tarih stili için .omid ifadesinin
+         geçirilmesi, yalnızca zaman bileşeninden
+         oluşan bir dize oluşturur.*/
 
-         Sistem, varsayılan bir stil kullanarak geçerli yerel ayar için tarih ve saatin dize gösterimini biçimlendirir. Tarih stili için .omid ifadesinin geçirilmesi, yalnızca zaman bileşeninden
-         oluşan bir dize oluşturur.
-         */
         let timeText = formatted(date: .omitted, time: .shortened)
-        
         /*
-         Uygulamanız, geçerli takvim günü için planlanan hatırlatıcılar için bir hatırlatıcının tarihini "Bugün saat 15:00" olarak görüntüleyerek aciliyet duygusu verecektir. Hatırlatıcının tarihi
-         geçerli takvim gününden başka bir şeyse, "22 Ekim saat 15:00" gibi bir dize kullanacaksınız.
+         Uygulamanız, geçerli takvim günü için planlanan hatırlatıcılar için bir hatırlatıcının tarihini "Bugün saat 15:00" olarak görüntüleyerek aciliyet
+         duygusu verecektir. Hatırlatıcının tarihi geçerli takvim gününden başka bir şeyse, "22 Ekim saat 15:00" gibi bir dize kullanacaksınız.
          
-         Bu tarihin geçerli takvim gününde olup olmadığını test eden bir if-else ifadesi ekleyin.
-         */
+         Bu tarihin geçerli takvim gününde olup olmadığını test eden bir if-else ifadesi ekleyin. */
         if Locale.current.calendar.isDateInToday(self) {
-            /*
-             Ardından, dünyanın dört bir yanındaki kullanıcılarınız için dizenin "Bugün at" bölümünü yerelleştireceksiniz. Uygulamanıza, her dize ve çevirisi için bir giriş içeren bir çeviri tablosu
-             ekleyeceksiniz.
+            /* Ardından, dünyanın dört bir yanındaki kullanıcılarınız için dizenin "Bugün at" bölümünü yerelleştireceksiniz. Uygulamanıza, her dize ve çevirisi
+            için bir giriş içeren bir çeviri tablosu ekleyeceksiniz.
              
-             Biçimlendirilmiş bir zaman dizesi oluşturmak için NSLocalizedString(_:comment:) işlevini çağırın ve sonucu timeFormat adlı bir sabite atayın.
+            Biçimlendirilmiş bir zaman dizesi oluşturmak için NSLocalizedString(_:comment:) işlevini çağırın ve sonucu timeFormat adlı bir sabite atayın.
 
-             Yorum parametresi, çevirmene, yerelleştirilmiş dizenin kullanıcıya sunumu hakkında bağlam sağlar.
-             */
+            Yorum parametresi, çevirmene, yerelleştirilmiş dizenin kullanıcıya sunumu hakkında bağlam sağlar. */
 
             let timeFormat = NSLocalizedString("Today at %@", comment: "Today at time format string")
-            
             // Zaman metnine timeFormat uygulayarak bir dize oluşturun ve sonucu döndürün.
             return String(format: timeFormat, timeText)
         } else {
-            /*
-             Daha sonra, sağlanan tarih geçerli takvim tarihinde olmadığında biçimlendirilmiş bir dize oluşturmak için Foundation çerçevesindeki formatted(date:time:) komutunu kullanacaksınız.
-
+            /* Daha sonra, sağlanan tarih geçerli takvim tarihinde olmadığında biçimlendirilmiş bir dize oluşturmak için Foundation çerçevesindeki
+             formatted(date:time:) komutunu kullanacaksınız.
+             
              Tarihin dize temsilini oluşturmak için formatted(_:) yöntemini çağırın ve sonucu dateText adlı bir sabite atayın.
+             
+             formatted(_:) yöntemi, kullanıcının geçerli yerel ayarına uygun bir tarih temsili oluşturmak için özel bir tarih formatı stili kullanır. */
 
-             formatted(_:) yöntemi, kullanıcının geçerli yerel ayarına uygun bir tarih temsili oluşturmak için özel bir tarih formatı stili kullanır.
-             */
             let dateText = formatted(.dateTime.month(.abbreviated).day())
-            
             // Tarih ve saati görüntülemek üzere biçimlendirilmiş bir dize oluşturmak için NSLocalizedString(_:comment:) işlevini çağırın ve sonucu dateAndTimeFormat adlı bir sabite atayın.
             let dateAndTimeFormat = NSLocalizedString("%@ at %@", comment: "Date and time format string")
-            
-            /*
-             Tarih ve saat metnine dateAndTimeFormat uygulayarak bir dize oluşturun ve sonucu döndürün.
-
-             Hesaplanan mülkünüz artık yerel ayarlara duyarlı bir biçimde tarih ve saati temsil ediyor.
-             */
-            return String(format: dateAndTimeFormat, dateText, timeText)
-            // NOT: Biçimlendirilmiş dizenin döndürülmesi derleyici hatasını düzeltir.
+            /* Tarih ve saat metnine dateAndTimeFormat uygulayarak bir dize oluşturun ve sonucu döndürün.
+             Hesaplanan mülkünüz artık yerel ayarlara duyarlı bir biçimde tarih ve saati temsil ediyor. */
+                       return String(format: dateAndTimeFormat, dateText, timeText)
+              // NOT: Biçimlendirilmiş dizenin döndürülmesi derleyici hatasını düzeltir.
+                   /* Yalnızca tarihin biçimlendirilmiş ve yerelleştirilmiş dize temsilini döndüren dayText adında başka bir hesaplanmış özellik oluşturun.
+                    Ardından aşağıdaki adımlarda özel tarih biçimi içeren bir çözüme bakın.
+                    
+                    Bu tarihin geçerli takvim gününde olması durumunda statik bir dize döndüren dayText adında hesaplanmış bir dize özelliği oluşturun. */
         }
-        /*
-         Yalnızca tarihin biçimlendirilmiş ve yerelleştirilmiş dize temsilini döndüren dayText adında başka bir hesaplanmış özellik oluşturun. Ardından aşağıdaki adımlarda özel tarih biçimi içeren bir
-         çözüme bakın.
-         
-         Bu tarihin geçerli takvim gününde olması durumunda statik bir dize döndüren dayText adında hesaplanmış bir dize özelliği oluşturun.
-         */
-        
+    }
         var dayText: String {
             if Locale.current.calendar.isDateInToday(self) {
-                return NSLocalizedString("String", comment: "Today due date description")
+                return NSLocalizedString("Today", comment: "Today due date description")
             }/*
               Not
               ------
@@ -90,4 +75,4 @@ extension Date {
         }
         
     }
-}
+
